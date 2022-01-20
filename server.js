b8dd5d8e267a8dcd67d36a3f373910d92449fc52
kamran saeidi <pakan0020@gmail.com>
2022-01-20 16:01:46 +0330

    Photo upload for bootcamp

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const express = require("express");
 const morgan = require("morgan");
 const errorHandler = require('./middleware/error');
 const dotenv = require("dotenv");
 const colors = require("colors");
+const fileupload = require('express-fileupload');
 
 const connectDB = require('./config/db')
 
@@ -27,6 +29,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// File uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname,'public')));
 
 // Mount routers
 const api = process.env.API;
