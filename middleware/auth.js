a1b3fe012f36a35cf1e8a7d750d8343daf490f75
kamran saeidi <pakan0020@gmail.com>
2022-01-28 01:42:46 +0330

    Role athorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,36 +1,51 @@
-const jwt = require('jsonwebtoken');
-const asyncHandler = require('./async');
-const ErrorResponse = require('../utils/errorResponse');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("./async");
+const ErrorResponse = require("../utils/errorResponse");
+const User = require("../models/User");
 
 // Protect routes
 exports.protect = asyncHandler(async (req, res, next) => {
-    let token;
+  let token;
 
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-    ) {
-        token = req.headers.authorization.split(' ')[1];
-    }
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
 
-    // else if(req.cookies.token) {
-    // token = req.cookies.token
-    // }
-    
-    // Make sure token exists
-    if(!token) {
-        return next(new ErrorResponse('Not authorize to access this route', 401));
-    }
+  // else if(req.cookies.token) {
+  // token = req.cookies.token
+  // }
 
-    try {
-        // Verify token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  // Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse("Not authorize to access this route", 401));
+  }
 
-        req.user = await User.findById(decoded.id);
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-       next();
-    } catch (err) {
-        return next(new ErrorResponse('Not authorize to access this route', 401));
-    }
+    req.user = await User.findById(decoded.id);
+
+    next();
+  } catch (err) {
+    return next(new ErrorResponse("Not authorize to access this route", 401));
+  }
 });
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
